2812e83d6f63337768fd6a13fda3767ff06a4076
Josh Zhang <zhangjos@amazon.com>
2019-06-22 16:10:28 -0700

    initial spring boot/maven project

---
+++ src/test/java/com/imaginationgame/demo/ImaginationGameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imaginationgame.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImaginationGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
