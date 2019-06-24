44ffe0bc30fe23fd3cc4552357ba84abdd5b25ce
Josh Zhang <josh.zhang91@gmail.com>
2019-06-23 22:08:58 -0700

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Imagination Game
+Oh yeah yeah
+Use java -jar target/demo-0.0.1-SNAPSHOT.jar (Runs the game)
+and ./mvnw package(Generates the jar file)
+(Use this through localhost:8080)
+por favor mi amigo.
--- src/main/java/com/imaginationgame/demo/TestController.java
+++ src/main/java/com/imaginationgame/demo/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot";
+        return "Gwee";
     }
 }
