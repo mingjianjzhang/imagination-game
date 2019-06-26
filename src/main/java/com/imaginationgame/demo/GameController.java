3d1e6d2e153a87a9c62b91f0cf9f5e9ce24f6a01
Josh Zhang <zhangjos@amazon.com>
2019-06-25 22:37:25 -0700

    testing form functionality

--- src/main/java/com/imaginationgame/demo/GameController.java
+++ src/main/java/com/imaginationgame/demo/GameController.java
@@ -3,7 +3,11 @@ package com.imaginationgame.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GameController {
@@ -12,5 +16,13 @@ public class GameController {
         model.addAttribute("name", name);
         return "home";
     }
+
+    @RequestMapping(value = "/createCharacter", method = RequestMethod.POST)
+    @ResponseBody
+    public String createCharater(@RequestParam(name="character_name") String characterName) {
+        System.out.println(characterName);
+        return "Nice";
+    }
+
 
 }
