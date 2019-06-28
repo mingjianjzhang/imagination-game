449d5935bbfb64aea0c0598b098dff11ce6f41ee
Samuel Zhang <epicknight653@gmail.com>
2019-06-27 22:50:01 -0700

    Changey the formey

--- src/main/java/com/imaginationgame/demo/GameController.java
+++ src/main/java/com/imaginationgame/demo/GameController.java
@@ -1,5 +1,5 @@
 package com.imaginationgame.demo;
-
+import java.util.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +19,36 @@ public class GameController {
 
     @RequestMapping(value = "/createCharacter", method = RequestMethod.POST)
     @ResponseBody
-    public String createCharater(@RequestParam(name="character_name") String characterName) {
-        System.out.println(characterName);
-        return "Nice";
+    public List<String> createCharater(
+    		/*Character Name has to be longer than one character
+    		 * Hp can't be negative or 0 and has to be less than 1000
+    		 * Ability name less than 20 chars.
+    		 * Ability damage cant be negative
+    		 */
+    		@RequestParam(name = "ability_description") String abiDesc,
+    		@RequestParam(name ="description") String charDescription, 
+    		@RequestParam(name = "ability_damage") int abilityDmg,
+    		@RequestParam(name ="ability_name") String abilityName,
+    		@RequestParam(name="character_name") String characterName,
+    		@RequestParam(name="character_health") int characterHP) {
+    	List<String> errorList = new ArrayList<String>();
+    	if(characterName.length() <= 1) {
+    		errorList.add("Name is too short");
+    	}
+    	if(characterHP <= 0 || characterHP > 1000 ) {
+    		errorList.add("Number error");
+    	}
+    	if(abilityName.length() > 20) {
+    		errorList.add("Name is too long");
+    	}
+    	if(abilityDmg < 0) {
+    		errorList.add("Please add damage to your move");
+    	}
+    	if(errorList.size() == 0) {
+    		errorList.add("u good buddy");
+    	}
+        System.out.println(errorList.toString());
+        return errorList;
     }
 
 
